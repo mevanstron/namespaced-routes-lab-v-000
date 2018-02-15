57a81d795559cd9abbdb6b0bff93a457f7fda795
mevanstron <michael@michaelgregoryevans.com>
2018-02-15 13:28:01 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :songs
+
+  namespace :admin do
+    resources :preferences
+  end
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
-  resources :songs
-
 end
